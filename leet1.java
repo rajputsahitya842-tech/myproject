9cc89f097ad85b861b849d0f85503c2d7b1079c3
tanish1008 <jaintanish031@gmail.com>
2026-03-13 00:35:23 +0530

    Add greeting from GitHub

--- leet1.java
+++ leet1.java
@@ -5,5 +5,6 @@ public class leet1 {
          System.out.println("Hello from tanish");
          System.out.println("Hello from Raja sab");
          System.out.println("Hello from koshik");
+        System.out.println(" hello from github");
     }
 }
