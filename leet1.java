f88f07bef3c80c52d40b9271b71d741fb51bc16c
sachinpatidar6035-blip <sachinpatidar6035@gmail.com>
2026-03-12 22:32:11 +0400

    Update leet1.java

--- leet1.java
+++ leet1.java
@@ -2,5 +2,7 @@ public class leet1 {
     public static void main(String[] args) {
         System.out.println("Hello from Sahitya");
          System.out.println("Hello from Sachin");
+         System.out.println("Hello from Sahitya");
+         System.out.println("Hello from Sachin");
     }
 }
